61a2186c6bf9fff3b2d566709fb69a63af4e33eb
vrugtehagel <vrugtehagel@gmail.com>
2020-04-21 09:17:26 +0200

    version 0.1.0; ad skip, popup away and mute are working

---
+++ background.js
@@ -0,0 +1,22 @@
+chrome.runtime.onInstalled.addListener(function(){
+	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+		chrome.declarativeContent.onPageChanged.addRules([{
+			conditions: [
+				new chrome.declarativeContent.PageStateMatcher({
+					pageUrl: {hostEquals: 'www.youtube.com'},
+				})
+			],
+			actions: [
+				new chrome.declarativeContent.ShowPageAction()
+			]
+		}]);
+	});
+
+	chrome.storage.sync.set({
+		mimicClick: true,
+		muteAds: false,
+		autoSkip: true,
+		autoClose: true,
+		autoImHere: true
+	});
+});
---
+++ content.js
@@ -0,0 +1,229 @@
+console.log('Convenient Youtube started.');
+(function(){
+	const log = (message, type) => {
+		return;
+		if(type == 'warn') console.warn(message);
+		else console.log(message);
+	}
+	const ytdApp = document.querySelector('ytd-app');
+	let muted = false;
+	let volume = null;
+	let userActive = true;
+	let adModuleObserver = null;
+	let fakeClickId = -1;
+	let options = {
+		mimicClick: true,
+		muteAds: false,
+		autoSkip: true,
+		autoClose: true,
+		autoImHere: true
+	};
+
+	const lookFor = ({
+		element: element,
+		config: config,
+		pattern: pattern,
+		ontrue: ontrue = () => {},
+		onfalse: onfalse = () => {}
+	}) => {
+		let patternFound = pattern();
+		const callback = value => {
+			if(value) ontrue();
+			else onfalse();
+		}
+		callback(patternFound);
+		const observer = new MutationObserver(() => {
+			if(pattern() == patternFound) return;
+			patternFound = !patternFound;
+			callback(patternFound);
+		});
+		observer.observe(element, config);
+		return observer;
+	};
+
+	const isHidden = element => getComputedStyle(element).display == 'none';
+
+	const clickButton = callback => {
+		//callback();
+		clearTimeout(fakeClickId);
+		fakeClickId = setTimeout(() => {
+			try{ callback(); }
+			catch(error){ log('Error: clicking failed.', 'warn'); }
+		}, 250);
+	};
+
+	const getChild = (element, classname) => {
+		const match = Array.from(element.children).find(child => child.classList.contains(classname));
+		if(match) return match;
+		return false;
+	};
+
+	const mute = () => {
+		if(!options.muteAds) return;
+		if(muted) log('I double muted something!', 'warn');
+		const video = document.querySelector('video.video-stream.html5-main-video');
+		if(!video) return;
+		if(video.volume) volume = video.volume;
+		video.volume = 0;
+		muted = true;
+	};
+
+	const unmute = () => {
+		if(!options.muteAds) return;
+		if(!muted) log('I double unmuted something!');
+		const video = document.querySelector('video.video-stream.html5-main-video');
+		if(!video) return;
+		if(!volume) return;
+		video.volume = volume;
+		muted = false;
+	};
+
+	// keep the options up to date
+	(() => {
+		chrome.storage.sync.get(['mimicClick', 'muteAds', 'autoSkip', 'autoClose', 'autoImHere'], result => {
+			for(let option in result) options[option] = result[option];
+		});
+
+		chrome.runtime.onMessage.addListener(message => {
+			for(let option in options){
+				options[option] = message[option];
+			}
+		});
+	})();
+
+	// check for user input to detect when the user is inactive
+	(() => {
+		let id = -1;
+		const resetTimer = () => {
+			clearTimeout(id);
+			userActive = true;
+			id = setTimeout(() => userActive = false, 10000);
+		};
+		['mousemove', 'mousedown', 'mouseup', 'scroll', 'mousewheel', 'keypress', 'touchstart', 'touchmove', 'touchend'].forEach(event => {
+			document.addEventListener(event, resetTimer);
+		});
+	})();
+
+	// look for mutations and changes on the page
+	lookFor({
+		element: ytdApp,
+		config: {attributes: true, childList: true, subtree: true},
+		pattern: () => {
+			if(!(ytdApp.hasAttribute('is-watch-page') || ytdApp.hasAttribute('miniplayer-active_'))) return false;
+			if(!ytdApp.querySelector('.video-ads.ytp-ad-module')) return false;
+			return true;
+		},
+		ontrue: () => {
+			log('You\'re watching a video.')
+			const adModule = ytdApp.querySelector('.video-ads.ytp-ad-module');
+			const popupContainer = ytdApp.querySelector('ytd-popup-container');
+			if(!adModule) return;
+			let unskippableAdFound = false;
+			adModuleObserver = lookFor({
+				element: adModule,
+				config: {childList: true},
+				pattern: () => !!adModule.children.length,
+				ontrue: () => {
+					log('\tAn ad has loaded.');
+					for(let i = 0; i < adModule.children.length; ++i){
+						const child = adModule.children[i];
+						if(child.classList.contains('ytp-ad-overlay-slot')){
+							log('\t\tIt\'s a popup ad (' + child.children[0].children.length + ' found),');
+							let foundAd = false;
+							for(let j = 0; j < child.children[0].children.length; ++j){
+								const ad = child.children[0].children[j];
+								if(isHidden(ad)) continue;
+								foundAd = true;
+								const closeButton = ad.querySelector('.ytp-ad-overlay-close-button');
+								clickButton(() => {
+									closeButton.click();
+									log('\t\t\tand I clicked it away');
+								});
+								break;
+							}
+							if(!foundAd) log('\t\t\tbut it was not visible, so I left it be');
+						}
+						if(child.classList.contains('ytp-ad-player-overlay')){
+							log('\t\tIt\'s a video ad,');
+							mute();
+							const persistingOverlay = getChild(adModule, 'ytp-ad-persisting-overlay');
+							if(persistingOverlay){
+								log('\t\t\tand it has a persisting overlay, whatever that means.');
+								const skipButtonSlot = persistingOverlay.querySelector('.ytp-ad-skip-button-slot');
+								lookFor({
+									element: skipButtonSlot,
+									config: {attributes: true},
+									pattern: () => !isHidden(skipButtonSlot),
+									ontrue: () => {
+										log('\t\t\t\tI see the skip button,');
+										clickButton(() => {
+											log('\t\t\t\tand I clicked it.');
+											skipButtonSlot.children[0].click();
+											unmute();
+										});
+									}
+								});
+							}
+							else {
+								log('\t\t\tand it look pretty normal.');
+								const skipButtonSlot = child.querySelector('.ytp-ad-skip-button-slot');
+								if(!skipButtonSlot){
+									log('\t\t\t\tbut there is no skip button.');
+									unskippableAdFound = true;
+									/*lookFor({
+										element: adModule,
+										config: {childList: true},
+										pattern: () => !!getChild(adModule, )
+									})*/
+								}
+								else{
+									lookFor({
+										element: skipButtonSlot,
+										config: {attributes: true},
+										pattern: () => !isHidden(skipButtonSlot),
+										ontrue: () => {
+											log('\t\t\t\tI see the skip button,');
+											clickButton(() => {
+												log('\t\t\t\tand I clicked it.');
+												skipButtonSlot.children[0].click();
+												unmute();
+											});
+										}
+									});
+								}
+							}
+						}
+					}
+				},
+				onfalse: () => {
+					if(unskippableAdFound){
+						unskippableAdFound = false;
+						unmute();
+					}
+					log('\tAll the ads are gone!');
+				}
+			});
+			lookFor({
+				element: popupContainer,
+				config: {childList: true},
+				pattern: () => (popupContainer.children.length == 1) && (document.hidden || !userActive),
+				ontrue: () => {
+					log('\tThere\'s a dialog on the screen that I don\'t quite like');
+					const buttons = popupContainer.children[0].querySelector('.buttons');
+					if(buttons.children.length > 2) return;
+					if(!options.autoImHere) return;
+					Array.from(buttons.children).forEach(button => {
+						if(button.children.length == 0) return;
+						button.click();
+						while(popupContainer.firstChild) popupContainer.removeChild(popupContainer.lastChild);
+						log('\t\tI tried to click it away.');
+					});
+				}
+			})
+		},
+		onfalse: () => {
+			log('You\'re browsing.')
+			if(adModuleObserver) adModuleObserver.disconnect();
+		}
+	});
+})();
