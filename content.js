a223a666ecdd2367ef18442ce2928ec7985ce483
vrugtehagel <vrugtehagel@gmail.com>
2020-06-20 13:15:29 +0200

    added extra requirements for clicking the 'are you still there' 'ok' button

--- content.js
+++ content.js
@@ -251,10 +251,12 @@ console.log('Convenient Youtube started.');
 				ontrue: () => {
 					unpause();
 					const buttons = popupContainer.querySelectorAll('yt-confirm-dialog-renderer .buttons > :not([hidden])');
-					if(buttons.length == 1){
-						buttons[0].click();
-						log('\tclicked away the "are you still there" window');
-					}
+					if(buttons.length != 1) return;
+					const button = buttons[0];
+					const rect = button.getBoundingClientRect();
+					if(!rect.width || !rect.height) return;
+					buttons[0].click();
+					log('\tclicked away the "are you still there" window');
 					//const annoyingDialog = Array.from(popupContainer.children).filter(dialog => !dialogs.includes(dialog))[0];
 					//console.log(dialogs, popupContainer.children, annoyingDialog);
 					//const buttons = annoyingDialog.querySelector('.buttons');
