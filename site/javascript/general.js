05da735dd1f35f70fd8e4e3fe48f51682c4d2ca7
vrugtehagel <vrugtehagel@gmail.com>
2020-06-26 13:11:54 +0200

    updated site

--- site/javascript/general.js
+++ site/javascript/general.js
@@ -1,9 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
 	const body = document.querySelector('body');
 	const readMoreButton = document.getElementById('read-more');
+	const main = document.querySelector('main');
 	const section = document.querySelector('section');
-
-	readMoreButton.addEventListener('click', () => fullMode(true));
 
 	const fullMode = bool => {
 		body.classList.toggle('full-mode', bool);
@@ -15,29 +14,45 @@ document.addEventListener('DOMContentLoaded', () => {
 		}
 	};
 
-	const isChrome = (() => {
-		const isChromium = window.chrome;
-		const vendor = window.navigator.vendor;
-		const isOpera = typeof window.opr !== "undefined";
-		const isIEedge = window.navigator.userAgent.indexOf("Edge") > -1;
-		const isIOSChrome = window.navigator.userAgent.match("CriOS");
-		if(isIOSChrome) return true;
-		return (isChromium !== null &&
-			typeof isChromium !== "undefined" &&
-			vendor === "Google Inc." &&
-			isOpera === false &&
-			isIEedge === false);
+	// check if the browser is Google Chrome
+	(() => {
+		const isChrome = (() => {
+			const isChromium = window.chrome;
+			const vendor = navigator.vendor;
+			const isOpera = window.opr !== undefined;
+			const isIEedge = navigator.userAgent.includes('Edge');
+			const isIOSChrome = navigator.userAgent.includes('CriOS');
+			if(isIOSChrome) return true;
+			if(isIEedge) return false;
+			if(isOpera) return false;
+			return isChromium && vendor == 'Google Inc.';
+		})();
+
+		if(!isChrome) body.classList.add('not-chrome');
 	})();
-	if(!isChrome) body.classList.add('not-chrome');
 
-	document.addEventListener('wheel', event => {
-		if(event.deltaY > 0) fullMode(true);
-		if(event.deltaY < 0) fullMode(false);
-	});
+	// set events that toggle or change full-mode
+	(() => {
+		// "read more" button
+		readMoreButton.addEventListener('click', () => fullMode(true));
 
-	document.addEventListener('keydown', event => {
-		if(event.key == 'Escape') return fullMode(false);
-		if(event.key == 'F11') return fullMode();
-		if(event.key == 'f') return fullMode();
-	});
+		// scrolling up and down
+		document.addEventListener('wheel', event => {
+			if(event.deltaY > 0) fullMode(true);
+			if(event.deltaY < 0) fullMode(false);
+		});
+
+		// escape quits fullscreen, F11 and f toggle it
+		document.addEventListener('keydown', event => {
+			if(event.key == 'Escape') return fullMode(false);
+			if(event.key == 'F11') return fullMode();
+			if(event.key == 'f') return fullMode();
+		});
+
+		// touchend event on the main (the video part) toggles it
+		main.addEventListener('touchend', () => {
+			if(event.target.classList.contains('download-button')) return;
+			fullMode();
+		});
+	})();
 });
